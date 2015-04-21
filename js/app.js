31c853e9ff67cd6fc78b6cd5e15ba1930f6d00a4
MellGitHub <mell.erades@gmail.com>
2015-04-21 10:53:12 +0200

    install angularJS and show notif channel

---
+++ js/app.js
@@ -0,0 +1,17 @@
+angular.module('chatApp', [])
+
+.controller('chatCtrl', function ($scope, $http, $sce) {
+    $scope.data = [];
+
+    $scope.toTrustedHTML = function( html ){
+        return $sce.trustAsHtml( html );
+    }
+
+    $http.get('../ajax.json').
+    success(function (data, status, headers, config) {
+        $scope.data = data;
+    }).
+    error(function (data, status, headers, config) {
+
+    });
+});
