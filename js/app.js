ded1cc5647d98df6447610e6dc5e65d894b274c1
MellGitHub <mell.erades@gmail.com>
2015-05-11 00:49:28 +0200

    up

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 var app = angular.module('chatApp', ['angular-gestures', 'ngRoute'], setPostHeader),
     firstConnexion = 0,
-    proxyURI = 'http://chat.dvp.io/',
+    proxyURI = 'http://chatp.dvp.io/',
     anoSmileys = {
       'smile.gif': ':)',
       'sad.gif': ':(',
