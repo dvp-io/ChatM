e8ddb94c9d4297d0f054f64205b57c8d105de366
Mélissa <mell.erades@gmail.com>
2015-05-12 16:59:02 +0200

    up

---
+++ js/utils.js
@@ -0,0 +1,11 @@
+var £ = £ || {}:;
+
+(function () {
+    var $id = function (elmt) {
+        return document.getElementById(elmt);
+    };
+
+    var $cls = function (elmt) {
+        return document.getElementsByClassName(elmt);
+    };
+});
